495e1c8e44151609b35f9b5e2f514f396acc668b
Moja One <joalex09@gmail.com>
2021-01-05 23:58:56 -0500

    app updates

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,132 +1,29 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#MongoDB and Flask Application"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#pip install Flask_Pymongo"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {
-    "scrolled": true
-   },
-   "outputs": [],
-   "source": [
-    "\n",
-    "# Dependencies and Setup\n",
-    "from flask import Flask, render_template\n",
-    "from flask_pymongo import PyMongo\n",
-    "import scrape_mars\n",
-    "import os"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#create an instance of Flask\n",
-    "app = Flask(__name__)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#mongodb connection\n",
-    "app.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/mars_app\"\n",
-    "mongo = PyMongo(app)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "@app.route(\"/\")\n",
-    "def index():\n",
-    "    mars = mongo.db.mars.find_one()\n",
-    "    return render_template(\"index.html\", mars = mars)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "\n",
-    "\n",
-    "@app.route(\"/scrape\")\n",
-    "def web_scrape():\n",
-    "    db.collection.remove({})\n",
-    "    mars_data = scrape.scrape()\n",
-    "    db.collection.insert_one(mars_data)\n",
-    "    return  render_template('scrape.html')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "if __name__ == \"__main__\":\n",
-    "    app.run()(debug=True)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+
+# Dependencies and Setup
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scrape_mars
+import os
+
+#create an instance of Flask
+app = Flask(__name__)
+
+#mongodb connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars)
+
+ 
+@app.route("/scrape")
+def web_scrape():
+    db.collection.remove({})
+    mars_data = scrape.scrape()
+    db.collection.insert_one(mars_data)
+    return  render_template('scrape.html')  
+
+    if __name__ == "__main__":
+    app.run()(debug=True) 
